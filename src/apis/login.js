6b60c4bed47a2790cd015e54965539125cdf5ada
Bao Trong <nhbaotrong@gmail.com>
2025-07-02 01:20:07 +0700

    feat: login

---
+++ src/apis/login.js
@@ -0,0 +1,23 @@
+import axiosClient from "./axiosClient"
+
+const login = async (email, password) => {
+     try {
+          return await axiosClient.post('/auth/login', {
+               email,
+               password
+          })
+     } catch (error) {
+          console.log(error)
+     }
+}
+
+const loginGoogle = async (accessToken) => {
+     try {
+          const response = await axiosClient.post('/auth/google', accessToken);
+          return response.data;
+     } catch (error) {
+          throw new Error(`Error: ${error}`);
+     }
+}
+
+export { login, loginGoogle }
--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import BlogDetail from '../screens/BlogDetail'
+import Login from '../screens/Login'
 import { RootStack } from './index'
 
 const Stack = createNativeStackNavigator()
@@ -9,6 +10,7 @@ export default function AppNavigator() {
      return (
           <NavigationContainer>
                <Stack.Navigator>
+                    <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
                     <Stack.Screen name="Root" component={RootStack} options={{ headerShown: false }} />
                     <Stack.Screen name="BlogDetail" component={BlogDetail} />
                </Stack.Navigator>
